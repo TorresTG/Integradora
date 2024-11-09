9dca3d4d620b80d6dcbfc9677def98c8ba316481
Torres <totilotegabriel@gmail.com>
2024-11-08 19:43:36 -0600

    tengo que salvar esto

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,9 +1,13 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import Tinaco from './tinaco.js'
+import Persona from './persona.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -13,9 +17,12 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
   declare id: number
-
+ 
   @column()
   declare fullName: string | null
+
+  @column()
+  public usuario!: string  //nombre del usuario
 
   @column()
   declare email: string
@@ -28,6 +35,16 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @hasMany(() => Tinaco, {
+    foreignKey: 'id_usuario',
+  })
+  public tinacos!: HasMany<typeof Tinaco>
+
+  @hasMany(() => Persona, {
+    foreignKey: 'id_usuario',
+  })
+  public personas!: HasMany<typeof Persona>
 
   static accessTokens = DbAccessTokensProvider.forModel(User)
 }
